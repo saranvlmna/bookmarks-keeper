0bdfce2f7693895767d1e788e0273a4d6b9132e8
saranjith <saranjith@metrictreelabs.com>
2022-11-20 15:29:31 +0530

    project init

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
+
+@Module({
+  imports: [AuthModule, BookmarkModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
